a29006f85f5c6aaa9a2692d67c82b0cdbedce683
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-06-29 08:38:09 +0700

    Hook Sort

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -37,7 +37,7 @@ export default {
     });
 
     watch(user, function () {
-      enteredSearchTerm.value = '';
+      updateSearch('')
     });
 
     return {
--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -14,8 +14,9 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 import useSearch from '../../hooks/search';
+import useSort from '../../hooks/sort';
 
 import UserItem from './UserItem.vue';
 
@@ -30,27 +31,7 @@ export default {
 
     const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName')
 
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { displayedUsers, sort, sorting } = useSort(availableItems, 'fullName')
 
     return {
       enteredSearchTerm,
